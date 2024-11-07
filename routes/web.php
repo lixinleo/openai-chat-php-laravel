cafb291900c90627d26888f906c2ef51912eeb11
lixinleo <lixinebo@yahoo.ca>
2024-11-06 21:43:37 -0800

    initate route

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('mychat.index');
 });
+
+Route::get('/mychat', function () {
+    return "hello world 2";
+})->name("mychat.index");
